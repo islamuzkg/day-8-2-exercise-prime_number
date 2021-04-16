4b083ca99ab75288df5e2c6ec8085f00d6ceb117
Islom Akhmedov <Islamuzkg@gmail.com>
2021-04-16 17:39:08 +0000

    updated function logic

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #Write your code below this line 👇
 def prime_checker(number):
-    prime_list=[]
-    for j in range(2, number+1):
-        if all(j % i != 0 for i in range(2, j)):
-            prime_list.append(j)
-            
-    if number in prime_list:
-        print(f"{number} is a prime number")
+    is_prime = True
+    for i in range(2, number):
+      if number % i == 0:
+        is_prime = False
+    
+    if is_prime: 
+      print("It's a prime number")
     else:
-        print(f"{number} is not a prime number")
+      print("It's not a prime number")
+
+
 
 
 
